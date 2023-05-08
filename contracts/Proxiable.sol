e07997972b1a324217c1ba472c4725bc2217854c
Mayank Saxena <mayank.saxena@zeeve.io>
2023-05-08 08:51:16 +0530

    REAVAX-27 | updateCodeAddress

---
+++ contracts/Proxiable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+
+import "./IERC1822.sol";
+
+contract Proxiable {
+    // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+    event UpdateCodeAddress(address indexed updatedAddress, address indexed sender);
+
+    function updateCodeAddress(address newAddress, address sender) internal {
+        require(
+            bytes32(
+                0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7
+            ) == Proxiable(newAddress).proxiableUUID(),
+            "Not compatible"
+        );
+        assembly {
+            // solium-disable-line
+            sstore(
+                0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7,
+                newAddress
+            )
+        }
+        emit UpdateCodeAddress(newAddress, sender);
+    }
+
+}
