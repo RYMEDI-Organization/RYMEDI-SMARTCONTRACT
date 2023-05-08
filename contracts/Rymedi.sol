4d59c3d7257fde01dfbb122fcc3d3490de1ac4a5
Mayank Saxena <mayank.saxena@zeeve.io>
2023-05-08 09:03:43 +0530

    REAVAX-43 | rymediInitialize

---
+++ contracts/Rymedi.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.19;
+import "./Proxiable.sol";
+
+/**
+ * @title Rymedi logic contract for Data storage
+ * @author Mayank Saxena (@forkblocks)
+ * @notice Use this contract to wite data to Proxy contract
+ */
+contract Rymedi is Proxiable, AccessControl, LibraryLock {
+    /// @dev variables
+    mapping(bytes32 => bytes32) records;
+    mapping(bytes32 => bytes32) deletedRecords;
+    bytes32[] recordKeyList;
+    bytes32[] deletedRecordKeys;
+    string[] roles;
+
+    /// @dev Owner - DEFAULT_ADMIN_ROLE + user roles
+    bytes32 public constant ADMIN = keccak256("ADMIN");
+    bytes32 public constant SENDER = keccak256("SENDER");
+
+    /// @dev events
+    event AddRecord(bytes32 indexed key, bytes32 indexed value);
+    event RemoveRecord(bytes32 indexed key, bytes32 indexed value);
+
+    // ========================================= Manage Rymedi Data =======================================================================
+
+    /*
+     * we will calculate the sha3 of the rymediInitialize() and we will pass the hash while deploying the proxy contract.
+     * This is like our constructor function.
+     * We are telling our proxy contract to call this function as contructor.
+     */
+    function rymediInitialize() public {
+        require(!initialized, "Already initalized");
+        initialize();
+        roles.push("DEFAULT_ADMIN_ROLE");
+        roles.push("ADMIN");
+        roles.push("SENDER");
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setRoleAdmin(SENDER, ADMIN);
+    }
+}
